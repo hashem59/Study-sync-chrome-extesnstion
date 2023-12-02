dd1068730cefb4e84351ee87315b7815864e5949
Hashem <111278402+hashemHP@users.noreply.github.com>
2023-12-02 13:15:15 +0200

    first draft design

--- background.js
+++ background.js
@@ -13,9 +13,9 @@
 // limitations under the License.
 
 chrome.runtime.onInstalled.addListener(() => {
-  chrome.action.setBadgeText({
+/*   chrome.action.setBadgeText({
     text: 'OFF'
-  });
+  }); */
 });
 
 const extensions = 'https://developer.chrome.com/docs/extensions';
@@ -23,7 +23,7 @@ const webstore = 'https://developer.chrome.com/docs/webstore';
 
 // When the user clicks on the extension action
 chrome.action.onClicked.addListener(async (tab) => {
-  if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
+/*   if (tab.url.startsWith(extensions) || tab.url.startsWith(webstore)) {
     // We retrieve the action badge to check if the extension is 'ON' or 'OFF'
     const prevState = await chrome.action.getBadgeText({ tabId: tab.id });
     // Next state will always be the opposite
@@ -48,5 +48,5 @@ chrome.action.onClicked.addListener(async (tab) => {
         target: { tabId: tab.id }
       });
     }
-  }
+  } */
 });
